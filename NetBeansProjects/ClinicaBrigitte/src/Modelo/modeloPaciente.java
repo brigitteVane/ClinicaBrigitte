9f6920d730f25d43c6f6e0a0bd68b0f6343533cc
brigitteVane <chavezvanessa442@gmail.com>
2023-06-22 16:54:33 -0500

    modelo paciente

--- NetBeansProjects/ClinicaBrigitte/src/Modelo/modeloPaciente.java
+++ NetBeansProjects/ClinicaBrigitte/src/Modelo/modeloPaciente.java
@@ -5,9 +5,68 @@
 package Modelo;
 
 /**
- *
- * @author invitado
+ * @author Brigitte Vanesa Chavez Riascos-2041700
  */
 public class modeloPaciente {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String tel;
+    private String dir;
+    
+    //constructor
+    public modeloPaciente (){
+        
+    }
+
+    public modeloPaciente(String id, String nombre, String apellidos, String tel, String dir) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.tel = tel;
+        this.dir = dir;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+    
+    
     
 }
